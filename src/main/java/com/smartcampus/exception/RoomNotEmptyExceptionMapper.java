b6cddfae43ec553c0a59b63fec2806bca5393b59
Oshadhi <oshadhiranawaka@gmail.com>
2026-04-19 14:13:21 +0530

    Implement RoomNotEmptyExceptionMapper to handle room deletion conflicts

--- src/main/java/com/smartcampus/exception/RoomNotEmptyExceptionMapper.java
+++ src/main/java/com/smartcampus/exception/RoomNotEmptyExceptionMapper.java
@@ -4,10 +4,30 @@
  */
 package com.smartcampus.exception;
 
-/**
- *
- * @author User
- */
-public class RoomNotEmptyExceptionMapper {
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+    
+    @Override
+    public Response toResponse(RoomNotEmptyException exception){
+        
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", 409);
+        error.put("error", "Conflict");
+        error.put("message", exception.getMessage());
+        error.put("hint", "Remove or reassign all sensors from this room before deleting it.");
+        
+        return Response
+                .status(Response.Status.CONFLICT) // 409
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
     
 }
