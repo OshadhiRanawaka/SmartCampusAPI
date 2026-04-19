c6245f65d767b04f5ac072cbd07356977f0df08e
Oshadhi <oshadhiranawaka@gmail.com>
2026-04-19 14:16:05 +0530

    Implement RoomNotFoundExceptionMapper to handle not found errors

--- src/main/java/com/smartcampus/exception/RoomNotFoundExceptionMapper.java
+++ src/main/java/com/smartcampus/exception/RoomNotFoundExceptionMapper.java
@@ -4,10 +4,28 @@
  */
 package com.smartcampus.exception;
 
-/**
- *
- * @author User
- */
-public class RoomNotFoundExceptionMapper {
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RoomNotFoundExceptionMapper implements ExceptionMapper<RoomNotFoundException>{
     
+    @Override
+    public Response toResponse(RoomNotFoundException exception){
+        
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", 404);
+        error.put("error", "Not Found");
+        error.put("message", exception.getMessage());
+
+        return Response
+                .status(Response.Status.NOT_FOUND) // 404
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
 }
